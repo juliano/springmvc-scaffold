d90cd5db2ed4f4d1ad3aafe01c7f19f0df1f733f
Juliano Alves <von.juliano@gmail.com>
2014-07-10 15:56:16 -0300

    runner CommandsHelp created

--- lib/springmvc-scaffold/runner/commands_help.rb
+++ lib/springmvc-scaffold/runner/commands_help.rb
@@ -2,6 +2,12 @@ module SpringMvcScaffold
   module Runner
 
     class CommandsHelp
+      def run(args)
+        Kernel.puts "Available runners for springmvc are: #{SpringMvcScaffold::COMMANDS.keys.join(', ')}"
+        Kernel.puts "Usage:"
+        Kernel.puts "  springmvc runner"
+      end
     end
+
   end
 end
