e9b2ece156f28c97c5adb4cfa29bd8f05aed0156
Juliano Alves <von.juliano@gmail.com>
2014-07-10 15:56:16 -0300

    runner CommandsHelp created

---
+++ spec/spring-scaffold/runner/commands_help_spec.rb
@@ -0,0 +1,13 @@
+require "spec_helper"
+
+describe SpringMvcScaffold::Runner::CommandsHelp do
+
+  it "should print commands help" do
+    expect(Kernel).to receive(:puts).with("Available runners for springmvc are: #{SpringMvcScaffold::COMMANDS.keys.join(', ')}")
+    expect(Kernel).to receive(:puts).with("Usage:")
+    expect(Kernel).to receive(:puts).with("  springmvc runner")
+
+    described_class.new.run('')
+  end
+
+end
