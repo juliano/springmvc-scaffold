1a171e2c1d35b96541fb5ab2bbcb7dc1b2695714
Juliano Alves <von.juliano@gmail.com>
2014-07-15 18:33:38 -0300

    creating constant for webapp

--- lib/springmvc-scaffold/configuration.rb
+++ lib/springmvc-scaffold/configuration.rb
@@ -1,3 +1,5 @@
 class Configuration
   MAIN_SRC = "src/main/java"
+  WEB_APP = "src/main/webapp"
+  WEB_INF = "#{WEB_APP}/WEB-INF"
 end
--- spec/spring-scaffold/configuration_spec.rb
+++ spec/spring-scaffold/configuration_spec.rb
@@ -6,4 +6,12 @@ describe Configuration do
     expect(Configuration::MAIN_SRC).to eq "src/main/java"
   end
 
+  it "should configure webapp" do
+    expect(Configuration::WEB_APP).to eq "src/main/webapp"
+  end
+
+  it "should configure web-inf" do
+    expect(Configuration::WEB_INF).to eq "#{Configuration::WEB_APP}/WEB-INF"
+  end
+
 end
