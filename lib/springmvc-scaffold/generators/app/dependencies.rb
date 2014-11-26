57c55431bcba4ccca59edd569a7d618b1c4bd664
Juliano Alves <von.juliano@gmail.com>
2014-11-26 12:09:10 -0200

    fixing spring libs

--- lib/springmvc-scaffold/generators/app/dependencies.rb
+++ lib/springmvc-scaffold/generators/app/dependencies.rb
@@ -2,7 +2,9 @@ class Dependencies
 
   def self.compile_scope
     hibernate_version = "4.0.1.Final"
-    [Dependency.new("org.springframework", "spring-web", "4.0.0"),
+    [Dependency.new("org.springframework", "spring-core", "4.1.2.RELEASE"),
+     Dependency.new("org.springframework", "spring-web", "4.1.2.RELEASE"),
+     Dependency.new("org.springframework", "spring-webmvc", "4.1.2.RELEASE"),
      Dependency.new("javax.servlet", "jstl", "1.2"),
      Dependency.new("org.hsqldb", "hsqldb", "2.2.8"),
      Dependency.new("org.hibernate", "hibernate-entitymanager", hibernate_version),
