fe1d650b43ff42bbd2623b0cbc06d94aaf8a4dbe
Juliano Alves <von.juliano@gmail.com>
2014-11-27 17:36:14 -0200

    creating model generator

--- lib/springmvc-scaffold/generators/base.rb
+++ lib/springmvc-scaffold/generators/base.rb
@@ -7,3 +7,4 @@ end
 require File.dirname(__FILE__) + '/app/app_generator'
 require File.dirname(__FILE__) + '/app/dependencies'
 require File.dirname(__FILE__) + '/app/dependency'
+require File.dirname(__FILE__) + '/scaffold/model/model_generator'
---
+++ lib/springmvc-scaffold/generators/scaffold/model/model_generator.rb
@@ -0,0 +1,2 @@
+class ModelGenerator
+end
