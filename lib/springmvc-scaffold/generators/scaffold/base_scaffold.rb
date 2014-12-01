64d8e64729e43bf2ed6b149a882a70078092826d
Juliano Alves <von.juliano@gmail.com>
2014-12-01 17:28:07 -0200

    defining controller and repository names for model

--- lib/springmvc-scaffold/generators/scaffold/base_scaffold.rb
+++ lib/springmvc-scaffold/generators/scaffold/base_scaffold.rb
@@ -11,4 +11,20 @@ class BaseScaffold < SpringMvcScaffold::Base
   def test_class_name
     "#{class_name}Test"
   end
+
+  def repository_class_name
+    "#{class_name}Repository"
+  end
+
+  def repository_test_class_name
+    "#{class_name}RepositoryTest"
+  end
+
+  def controller_class_name
+    "#{class_name}Controller"
+  end
+
+  def controller_test_class_name
+    "#{class_name}ControllerTest"
+  end
 end
--- spec/spring-scaffold/generators/scaffold/base_scaffold_spec.rb
+++ spec/spring-scaffold/generators/scaffold/base_scaffold_spec.rb
@@ -11,5 +11,21 @@ describe BaseScaffold do
     it "should know model test class name" do
       expect(subject.test_class_name).to eq "ClientTest"
     end
+
+    it "should know repository class name" do
+      expect(subject.repository_class_name).to eq "ClientRepository"
+    end
+
+    it "should know repository test class name" do
+      expect(subject.repository_test_class_name).to eq "ClientRepositoryTest"
+    end
+
+    it "should know controller class name" do
+      expect(subject.controller_class_name).to eq "ClientController"
+    end
+
+    it "should know controller test class name" do
+      expect(subject.controller_test_class_name).to eq "ClientControllerTest"
+    end
   end
 end
