32e5d2cbe8cf4e66c54e4359d4db059a66dfe0e1
Juliano Alves <von.juliano@gmail.com>
2014-12-02 13:12:20 -0200

    testing valid types

--- spec/spring-scaffold/generators/scaffold/attribute_spec.rb
+++ spec/spring-scaffold/generators/scaffold/attribute_spec.rb
@@ -25,4 +25,46 @@ describe Attribute do
       expect(described_class.new("value", "DoublE").type).to eq "double"
     end
   end
+
+  context "supported types" do
+    it "should support boolean" do
+      expect(described_class::VALID_TYPES.include?("boolean")).to be true
+    end
+
+    it "should support double" do
+      expect(described_class::VALID_TYPES.include?("double")).to be true
+    end
+
+    it "should support string" do
+      expect(described_class::VALID_TYPES.include?("string")).to be true
+    end
+
+    it "should support float" do
+      expect(described_class::VALID_TYPES.include?("float")).to be true
+    end
+
+    it "should support short" do
+      expect(described_class::VALID_TYPES.include?("short")).to be true
+    end
+
+    it "should support integer" do
+      expect(described_class::VALID_TYPES.include?("integer")).to be true
+    end
+
+    it "should support long" do
+      expect(described_class::VALID_TYPES.include?("long")).to be true
+    end
+
+    it "should support text" do
+      expect(described_class::VALID_TYPES.include?("text")).to be true
+    end
+
+    it "should not support other" do
+      expect(described_class::VALID_TYPES.include?("other")).to be false
+    end
+
+    it "should suport date" do
+      expect(described_class::VALID_TYPES.include?("date")).to be true
+    end
+  end
 end
