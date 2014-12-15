188f44d8ea89a14df528f49dda3c4dd6d922c3d7
Juliano Alves <von.juliano@gmail.com>
2014-12-15 14:11:19 -0200

    adding @Autowired to the template

--- spec/spring-scaffold/generators/scaffold/repository/templates/Clients.java
+++ spec/spring-scaffold/generators/scaffold/repository/templates/Clients.java
@@ -1,6 +1,7 @@
 package app.repositories;
 
 import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import app.models.Client;
@@ -8,6 +9,7 @@ import app.models.Client;
 @Repository
 public class Clients extends GenericRepository<Client, Long> {
 
+	@Autowired
 	public Clients(Session session) {
 		super(session);
 	}
--- spec/spring-scaffold/generators/scaffold/repository/templates/Products.java
+++ spec/spring-scaffold/generators/scaffold/repository/templates/Products.java
@@ -1,6 +1,7 @@
 package app.repositories;
 
 import javax.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import app.models.Product;
@@ -8,6 +9,7 @@ import app.models.Product;
 @Repository
 public class Products extends GenericRepository<Product, Long> {
 
+	@Autowired
 	public Products(EntityManager entityManager) {
 		super(entityManager);
 	}
