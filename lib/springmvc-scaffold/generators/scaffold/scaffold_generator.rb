7eec88e9798d3a5842600440e5a387f541463593
Juliano Alves <von.juliano@gmail.com>
2015-04-03 18:07:53 -0300

    scaffold generator

--- lib/springmvc-scaffold/generators/scaffold/scaffold_generator.rb
+++ lib/springmvc-scaffold/generators/scaffold/scaffold_generator.rb
@@ -1,2 +1,27 @@
-class ScaffoldGenerator
+class ScaffoldGenerator < SpringMvcScaffold::Base
+
+  attr_accessor :generated_attributes
+  argument :model
+  argument :attributes, type: :hash, default: {}, banner: "field:type field:type"
+
+  def initialize(args)
+    super(args)
+    @generated_attributes = attributes.map {|field, type| Attribute.new(field, type) }
+  end
+
+  def controller_generator
+    ControllerGenerator.new(model, generated_attributes).build
+  end
+
+  def model_generator
+    ModelGenerator.new(model, generated_attributes).build
+  end
+
+  def template_generator
+    JspGenerator.new(model, generated_attributes).build
+  end
+
+  def repository_generator
+    RepositoryGenerator.new(model, generated_attributes).build
+  end
 end
--- spec/springmvc-scaffold/generators/scaffold/scaffold_generator_spec.rb
+++ spec/springmvc-scaffold/generators/scaffold/scaffold_generator_spec.rb
@@ -1,4 +1,60 @@
 require File.expand_path(File.dirname(__FILE__) + "/../../../spec_helper")
 
 describe ScaffoldGenerator do
+  let(:model) { "product" }
+  let(:args) { [model, { "name" => "string", "value" => "double" }] }
+  subject { described_class.new(args) }
+  let(:attributes) { subject.generated_attributes }
+
+  context "generated attributes" do
+    it "generates name attribute" do
+      att = attributes.first
+      expect(att.name).to eq "name"
+      expect(att.type).to eq "string"
+    end
+
+    it "generates value attribute" do
+      att = attributes.last
+      expect(att.name).to eq "value"
+      expect(att.type).to eq "double"
+    end
+  end
+
+  context "generators" do
+    it "calls controller generator" do
+      controller_generator = double(ControllerGenerator)
+      allow(ControllerGenerator).to receive(:new).with(model, attributes).and_return(controller_generator)
+      expect(controller_generator).to receive(:build)
+
+      subject.controller_generator
+    end
+
+    it "calls model generator" do
+      config = {}
+      allow(YAML).to receive(:load_file).with(Configuration::FILENAME).and_return(config)
+      model_generator = double(ModelGenerator)
+      allow(ModelGenerator).to receive(:new).with(model, attributes).and_return(model_generator)
+      expect(model_generator).to receive(:build)
+
+      subject.model_generator
+    end
+
+    it "calls jsp generator" do
+      template_generator = double(JspGenerator)
+      allow(JspGenerator).to receive(:new).with(model, attributes).and_return(template_generator)
+      expect(template_generator).to receive(:build)
+
+      subject.template_generator
+    end
+
+    it "calls repository generator" do
+      config = {}
+      allow(YAML).to receive(:load_file).with(Configuration::FILENAME).and_return(config)
+      repository_generator = double(RepositoryGenerator)
+      allow(RepositoryGenerator).to receive(:new).with(model, attributes).and_return(repository_generator)
+      expect(repository_generator).to receive(:build)
+
+      subject.repository_generator
+    end
+  end
 end
