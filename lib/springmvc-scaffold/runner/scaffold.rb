e24bcc97358ff04be6668380a14912a8c992f338
Juliano Alves <von.juliano@gmail.com>
2015-04-03 18:41:13 -0300

    created scaffold runner

--- lib/springmvc-scaffold/runner/scaffold.rb
+++ lib/springmvc-scaffold/runner/scaffold.rb
@@ -2,6 +2,14 @@ module SpringMvcScaffold
   module Runner
 
     class Scaffold
+      def run(args)
+        if File.exists?("src")
+          ScaffoldGenerator.new(args).invoke_all
+        else
+          Kernel.puts "To run springmvc scaffold please go to the project root folder."
+        end
+      end
     end
+
   end
 end
--- spec/springmvc-scaffold/runner/scaffold_spec.rb
+++ spec/springmvc-scaffold/runner/scaffold_spec.rb
@@ -1,4 +1,32 @@
 require 'spec_helper'
 
 describe SpringMvcScaffold::Runner::Scaffold do
+
+  context "scaffold generator" do
+    let(:generator) { double(ScaffoldGenerator) }
+    let(:args) { ["product", "name:string", "value:double"] }
+
+    it "invokes all scaffold generator tasks" do
+      allow(File).to receive(:exists?).and_return(true)
+      allow(ScaffoldGenerator).to receive(:new).with(args).and_return(generator)
+      expect(generator).to receive(:invoke_all)
+
+      subject.run(args)
+    end
+
+    context "outside root folder" do
+      before { allow(File).to receive(:exists?).and_return(false) }
+
+      it "cannot invoke scaffold generator" do
+        expect(ScaffoldGenerator).to_not receive(:new)
+        subject.run(args)
+      end
+
+      it "asks user to go to root folder" do
+        expect(Kernel).to receive(:puts).with("To run springmvc scaffold please go to the project root folder.")
+        subject.run(args)
+      end
+    end
+  end
+
 end
